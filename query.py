8ae15d2eaef5eba7a78be25b03685970c5a452fd
vgg5465 <37584084+vgg5465@users.noreply.github.com>
2022-11-08 16:50:42 +0100

    Update query.py

--- query.py
+++ query.py
@@ -1,14 +1,14 @@
 import requests
 
 def query(titolo):
-  url = 'https://it.wikipedia.org/w/api.php?action=parse&format=json&page=Avatar%20(film%202009)&prop=wikitext&section=1&disabletoc=1&formatversion=2'
-
+  #Function that create the link for the query and return the json
   titolo_replaced = titolo.replace(" ","%20")
   
+  #Base link created with wp special page: https://it.wikipedia.org/wiki/Speciale:ApiSandbox
   url = 'https://it.wikipedia.org/w/api.php?action=parse&format=json&page='+titolo_replaced+'&prop=wikitext&section=1&disabletoc=1&formatversion=2'
   
   headers = {
-    'Authorization': 'PASTE YOUR KEY',
+    'Authorization': ' /* PASTE YOUR KEY HERE *\ ',
     'User-Agent': 'plot lenght check'
   }
   
